5cffebbbe8374ce6703c27b6ea38e5644cadce6e
Maria Małasiewicz <79585515+malasie@users.noreply.github.com>
2024-10-26 19:10:20 +0200

    zad3

---
+++ zadanie3/angielski.py
@@ -0,0 +1,22 @@
+import nltk
+nltk.download('words')
+
+from nltk.corpus import words
+word_list = words.words()
+
+import regex as re
+
+reg= re.compile(r"(.)\1")
+dl = {}
+
+for word in word_list:
+    match = re.search(reg, word)
+    if match:
+        if match.group(1) not in dl:
+            dl[match.group(1)] =1
+            
+        else:
+            dl[match.group(1)]+=1
+    
+        
+print(dl)
