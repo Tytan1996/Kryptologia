50f79cf8e7d7f4c550236314e6e91ab9fd651c34
Maria Małasiewicz <79585515+malasie@users.noreply.github.com>
2024-11-13 23:29:13 +0100

    interfejs

--- zadanie4/interfejs.py
+++ zadanie4/interfejs.py
@@ -11,40 +11,43 @@ def interfejs():
     nazwa_pliku= input("Podaj nazwę pliku (.txt) do wczytania: ")
     if nazwa_pliku[-4:]!='.txt':
         nazwa_pliku+='.txt'
-        
-    dane=wczytajPlik(nazwa_pliku)
-    if len(dane)!=2:
-        while True:
-            opcja=input("By zaszyfrować tekst wprowadź: 1,\nBy odszyfrować wprowadź: 2\n")
-            if opcja in ['1', '2']:
-                break
-            else:
-                print('Wybrano złą opcję, spróbuj jeszcze raz.')
-        klucz=input("Podaj klucz: ")
-        tekst=dane
+    try:    
+        dane=wczytajPlik(nazwa_pliku)
+    except FileNotFoundError:
+        pass
     else:
-        tekst, klucz = dane
-        opcja='1'
-      
-    print("Wybierz, co uwzględnić:\n1. polskie litery,\n2. spacje,\n3. cyfry\n\nnp. by wybrać wszystkie opcje wpisz: 123")
-    kod=['0']*3
-    try:
-        opcje=input()
-        for i in range(len(opcje)):
-            kod[int(opcje[i])-1]='1'
+        if len(dane)!=2:
+            while True:
+                opcja=input("By zaszyfrować tekst wprowadź: 1,\nBy odszyfrować wprowadź: 2\n")
+                if opcja in ['1', '2']:
+                    break
+                else:
+                    print('Wybrano złą opcję, spróbuj jeszcze raz.')
+            klucz=input("Podaj klucz: ")
+            tekst=dane
+        else:
+            tekst, klucz = dane
+            opcja='1'
+          
+        print("Wybierz, co uwzględnić:\n1. polskie litery,\n2. spacje,\n3. cyfry\n\nnp. by wybrać wszystkie opcje wpisz: 123")
+        kod=['0']*3
+        try:
+            opcje=input()
+            for i in range(len(opcje)):
+                kod[int(opcje[i])-1]='1'
+            
+        except:
+            print('Wprowadzono złe wartosci, szyfrowanie bez dodatkowych opcji')
+        if opcja=='1':
+            wynik=vigenere_cipher2(tekst, klucz, int(kod[0]), int(kod[1]), int(kod[2]))
+            
+        elif opcja=='2':
+            kod=''.join(kod)
+            wynik=vigenere_decipher(tekst, klucz, kod)
         
-    except:
-        print('Wprowadzono złe wartosci, szyfrowanie bez dodatkowych opcji')
-    if opcja=='1':
-        wynik=vigenere_cipher2(tekst, klucz, int(kod[0]), int(kod[1]), int(kod[2]))
+        nazwa = input('Podaj nazwę pliku do zapisu: ')
+        zapisPliku(wynik, nazwa)
         
-    elif opcja=='2':
-        kod=''.join(kod)
-        wynik=vigenere_decipher(tekst, klucz, kod)
-    
-    nazwa = input('Podaj nazwę pliku do zapisu: ')
-    zapisPliku(wynik, nazwa)
-    
-    print('Zapisano')
+        print('Zapisano')
     
 interfejs()
